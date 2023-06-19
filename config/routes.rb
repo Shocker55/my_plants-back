8269aff1d0798f38fcb4105ff32e6a3c3cad628e
Shocker55 <bialetti360@gmail.com>
2023-06-19 20:02:29 +0900

    feat: ブログ記事の一覧・詳細・作成APIの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/health_check', to: 'health_check#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :blogs, only: %i[index show create]
 end
