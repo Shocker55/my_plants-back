9a58d0b13179e2c13c4af940e1200d498f32a9aa
Shocker55 <bialetti360@gmail.com>
2023-06-20 21:50:21 +0900

    fix: spell

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/health_check', to: 'health_check#index'
+  get '/health_check', to: 'health_checks#index'
 
   resources :blogs, only: %i[index show create]
 end
