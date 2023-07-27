ec12e2290eaf39908bd391a87f61f6a44b28f313
Shocker55 <bialetti360@gmail.com>
2023-07-27 19:55:41 +0900

    feat: 開発環境でのDockerの設定の追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_19_105637) do
-  create_table "blogs", force: :cascade do |t|
+  create_table "blogs", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "contents"
     t.datetime "created_at", null: false
