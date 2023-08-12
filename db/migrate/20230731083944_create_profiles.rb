1d78092f21b0d716d9a2bccce0ee23e10849692e
Shocker55 <bialetti360@gmail.com>
2023-08-13 05:30:06 +0900

    fix: profilesテーブルのavatarカラムの修正

--- db/migrate/20230731083944_create_profiles.rb
+++ db/migrate/20230731083944_create_profiles.rb
@@ -2,11 +2,13 @@ class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :profiles do |t|
       t.string :name,    null: false
-      t.string :avatar,  null: false
       t.text :bio,       null: false
+      t.string :avatar
       t.integer :role, default: 0, null: false
-      t.references :user, null: false, index: { unique: true }, foreign_key: true
+
       t.timestamps
+
+      t.references :user, null: false, index: { unique: true }, foreign_key: true
     end
   end
 end
