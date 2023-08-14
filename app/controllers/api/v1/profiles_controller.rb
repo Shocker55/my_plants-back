67ddbbbc0b11c07e7a2bf54c24758b1ec70ba5cd
Shocker55 <bialetti360@gmail.com>
2023-08-14 20:43:18 +0900

    feat: Profileコントローラにupdateアクションの追加、Recordコントローラの関連付けの修正

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -9,15 +9,22 @@ class Api::V1::ProfilesController < ApplicationController
   end
 
   def create
-    @profile = current_user.build_profile(profile_params)
-    if @profile.save
+    profile = current_user.build_profile(profile_params)
+    if profile.save
       head :created
     else
-      render json: { message: @profile.errors.to_hash(true) }, status: 422
+      render json: { message: profile.errors.to_hash(true) }, status: 422
     end
   end
 
-  def edit; end
+  def update
+    profile = Profile.find(current_user.id)
+    if profile.update(profile_params)
+      head :created
+    else
+      render json: { message: profile.errors.to_hash(true) }, status: 422
+    end
+  end
 
   private
 
--- app/models/related_record.rb
+++ app/models/related_record.rb
@@ -1,6 +1,6 @@
 class RelatedRecord < ApplicationRecord
   belongs_to :record
 
-  validates :record_id, presence: true, numericality: { greater_than: 0 }
-  validates :related_record_id, presence: true, uniqueness: true
+  validates :record_id, presence: true, uniqueness: true
+  validates :related_record_id, presence: true, numericality: { greater_than: 0 }
 end
