d227a2a1faec2c61765305050c8b5dd5f2a0c83c
Shocker55 <bialetti360@gmail.com>
2023-09-28 07:34:33 +0900

    fix: イベント作成画面でエラー表示がされないバグの修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -21,7 +21,8 @@ class Event < ApplicationRecord
   end
 
   def start_end_date_check
-    errors.add(:end_date, "は開始日より前の日付は登録できません") unless
-    start_date <= end_date
+    if start_date && end_date && start_date > end_date
+      errors.add(:end_date, "は開始日より前の日付は登録できません")
+    end
   end
 end
